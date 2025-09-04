2d55ecb3a53695660a5c33d48e771080e4fc7ec3
daten_krake <dat3n_krak3@pm.me>
2025-09-04 16:42:50 +0200

    started csv impl

--- defender_onboarding/main.go
+++ defender_onboarding/main.go
@@ -1,7 +1,8 @@
 package main
- 
+
 import (
 	b64 "encoding/base64"
+	"encoding/csv"
 	"encoding/json"
 	"flag"
 	"fmt"
@@ -15,41 +16,44 @@ import (
 
 	"github.com/joho/godotenv"
 )
-type onboardinScript struct{
-		ID          string  `json:"id"`
-		Name        string  `json:"name"`
-		Type        string  `json:"type"`
-		Location    string  `json:"location"`
-		Properties  json.RawMessage  `json:"properties"`
-	}
 
-type packages struct{
-	Windows    string  `json:"onboardingPackageWindows"`
-	Linux      string  `json:"onboardingPackageLinux"`
-	WindowsCM  string  `json:"onboardingPackageWindowsCM"`
-	Winverify  json.RawMessage `json:"windowsVerificationModel"`
-	Linuxverify  json.RawMessage `json:"linuxVerificationModel"`
+type onboardinScript struct {
+	ID         string          `json:"id"`
+	Name       string          `json:"name"`
+	Type       string          `json:"type"`
+	Location   string          `json:"location"`
+	Properties json.RawMessage `json:"properties"`
+}
+
+type packages struct {
+	Windows     string          `json:"onboardingPackageWindows"`
+	Linux       string          `json:"onboardingPackageLinux"`
+	WindowsCM   string          `json:"onboardingPackageWindowsCM"`
+	Winverify   json.RawMessage `json:"windowsVerificationModel"`
+	Linuxverify json.RawMessage `json:"linuxVerificationModel"`
 	WinCMverify json.RawMessage `json:"windowsCMVerificationModel"`
 }
 type tokenResponse struct {
-		TokenType    string `json:"token_type"`
-		ExpiresIn    string `json:"expires_in"`
-		ExtExpiresIn string `json:"ext_expires_in"`
-		ExpiresOn    string `json:"expires_on"`
-		NotBefore    string `json:"not_before"`
-		Resource     string `json:"resource"`
-		AccessToken  string `json:"access_token"`
-	}
+	TokenType    string `json:"token_type"`
+	ExpiresIn    string `json:"expires_in"`
+	ExtExpiresIn string `json:"ext_expires_in"`
+	ExpiresOn    string `json:"expires_on"`
+	NotBefore    string `json:"not_before"`
+	Resource     string `json:"resource"`
+	AccessToken  string `json:"access_token"`
+}
 
 var tenantID string
 var appID string
 var secret string
 var token string
 var subid string
+var subidlist []string
+var list string
 
-func init(){
-	err :=  godotenv.Load()
-	if err != nil{
+func init() {
+	err := godotenv.Load()
+	if err != nil {
 		fmt.Println("error loading .env")
 	}
 	tenantID = os.Getenv("TENANTID")
@@ -57,29 +61,32 @@ func init(){
 	secret = os.Getenv("SECRET")
 }
 
-func main(){
+func main() {
 
-	tenant := flag.String("tenant_id",tenantID,"Set Tenant ID like XXX")
-	flag.StringVar(&subid,"subid","subID","Please set the subscription ID")
-	outPath := flag.String("path",".","Set output Path")
-	singleRun := flag.Bool("singleRun",false,"set false or true")
-	//csvList := flag.String("csv","0","Set  CSV Files for multiple Tenants")
+	flag.StringVar(&subid, "subid", "subID", "Please set the subscription ID")
+	//outPath := flag.String("path",".","Set output Path")
+	flag.StringVar(&list, "csv", "0", "Set  CSV Files for multiple Tenants")
 	flag.Parse()
-	fmt.Println(tenant,outPath,singleRun)		
 
 	//  token move
-	token1,err := GetToken(tenantID,appID,secret)
-	if err != nil{
+	token1, err := GetToken(tenantID, appID, secret)
+	if err != nil {
 		log.Fatal(err)
 	}
 	token = token1
-	//write out the package
-	winpackage := parsePackage(GetOnboardingPackage(token))
-	decodedwinpackage,_:= b64.StdEncoding.DecodeString(winpackage)
-	os.WriteFile(tenantID+"_"+"onboarding.cmd",[]byte(decodedwinpackage),0644)
+	readSubCsv(list)
+	if len(list) < 2 {
+		//write out the package
+		winpackage := parsePackage(GetOnboardingPackage(token))
+		decodedwinpackage, _ := b64.StdEncoding.DecodeString(winpackage)
+		os.WriteFile(tenantID+"_"+"onboarding.cmd", []byte(decodedwinpackage), 0644)
+	} else {
+		for i := range list {
+			fmt.Print(list[i])
+		}
+	}
+
 }
-
-
 
 func GetToken(tenant string, clientID string, clientSecret string) (string, error) {
 
@@ -122,15 +129,14 @@ func GetToken(tenant string, clientID string, clientSecret string) (string, erro
 	return tokenResponse.AccessToken, nil
 }
 
-
-func GetOnboardingPackage(token string) []byte{
-	baseUrl := fmt.Sprintf("https://management.azure.com/subscriptions/%s/providers/Microsoft.Security/mdeOnboardings/default?api-version=2021-10-01-preview",subid) 
+func GetOnboardingPackage(token string) []byte {
+	baseUrl := fmt.Sprintf("https://management.azure.com/subscriptions/%s/providers/Microsoft.Security/mdeOnboardings/default?api-version=2021-10-01-preview", subid)
 	bearer := "bearer " + token
-	req,err := http.NewRequest("GET",baseUrl,nil)
-	if err != nil{
+	req, err := http.NewRequest("GET", baseUrl, nil)
+	if err != nil {
 		log.Fatal(err)
 	}
-	req.Header.Add("Authorization" ,bearer)
+	req.Header.Add("Authorization", bearer)
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
@@ -138,32 +144,48 @@ func GetOnboardingPackage(token string) []byte{
 	}
 	defer resp.Body.Close()
 	body, err := io.ReadAll(resp.Body)
-	if err != nil{
+	if err != nil {
 		log.Fatal(err)
 	}
 	return []byte(body)
-	}
+}
 
-func parsePackage(onpackage []byte) (string){
+func parsePackage(onpackage []byte) string {
 
 	var oP onboardinScript
-	var opS packages 
+	var opS packages
 
-	if strings.Contains(string([]byte(onpackage)), "error"){	
+	if strings.Contains(string([]byte(onpackage)), "error") {
 		fmt.Println("Error occured on the backend, wait 1min and retry")
 		fmt.Println(string([]byte(onpackage)))
-		time.Sleep(time.Second*60)
+		time.Sleep(time.Second * 60)
 		return parsePackage(GetOnboardingPackage(token))
-	}else{
+	} else {
 		err := json.Unmarshal(onpackage, &oP)
-		if err != nil{
+		if err != nil {
 			log.Fatal(err)
 		}
 		pack := json.Unmarshal(oP.Properties, &opS)
-		if pack != nil{
+		if pack != nil {
 			log.Fatal(pack)
 		}
-	}	
+	}
 	return string(opS.Windows)
 
+}
+
+func readSubCsv(list string) {
+	file, err := os.Open(list)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	csvReader := csv.NewReader(file)
+	subids, err := csvReader.ReadAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+	for id := range subid {
+		subidlist = append(subidlist, subids[0][id])
+	}
 }
